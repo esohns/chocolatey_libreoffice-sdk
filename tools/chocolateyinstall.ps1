8a980edf4979dc961b2032936c4b63eb8a06636e
Erik Sohns <erik.sohns@posteo.de>
2025-09-02 18:20:41 +0200

    update to 25.2.5

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,12 +4,12 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName            = 'libreoffice-sdk'
-  version                = '25.2.4'
+  version                = '25.2.5'
   fileType               = 'msi'
-  url                    = 'https://download.documentfoundation.org/libreoffice/stable/25.2.4/win/x86/LibreOffice_25.2.4_Win_x86_sdk.msi'
-  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/25.2.4/win/x86_64/LibreOffice_25.2.4_Win_x86-64_sdk.msi'
-  checksum               = 'cd04cb94b6cd0bccfaa3b99e680f2c8b52f38c5ffc6b5a96843fc741f1ac4036'
-  checksum64             = '85a73b6be35bb0260b5e1b85cd9d2de7a1437b3616536dc026dadd64f8169935'
+  url                    = 'https://download.documentfoundation.org/libreoffice/stable/25.2.5/win/x86/LibreOffice_25.2.5_Win_x86_sdk.msi'
+  url64bit               = 'https://download.documentfoundation.org/libreoffice/stable/25.2.5/win/x86_64/LibreOffice_25.2.5_Win_x86-64_sdk.msi'
+  checksum               = '6853afd8476a12711bd28169019424552d7d546be3e2d834e45d0635494f9a18'
+  checksum64             = '79181eeb1d52dff12bee24cd2f86141769357b6fcb51a8cd8a2041160bb25e02'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
